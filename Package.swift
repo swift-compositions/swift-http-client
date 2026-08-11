6601c1cb869b468123d19979c9d78e20d4a11718
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-11 16:44:47 +0200

    Pin final HTTP Body and IO revisions

--- Package.swift
+++ Package.swift
@@ -7,9 +7,9 @@ let package = Package(
     platforms: [.macOS(.v26), .iOS(.v26), .tvOS(.v26), .watchOS(.v26), .visionOS(.v26)],
     products: [.library(name: "HTTP Client", targets: ["HTTP Client"])],
     dependencies: [
-        .package(url: "https://github.com/swift-foundations/swift-http-body.git", revision: "1d3bb5ffb1fa4a9e429c863a0af7416f91355901"),
+        .package(url: "https://github.com/swift-foundations/swift-http-body.git", revision: "0f670ceb05701fbaaea9141342ba295d20ca4ee8"),
         .package(url: "https://github.com/swift-primitives/swift-pool-primitives.git", revision: "b7c710c945b7c8467b4521c3a2d5b00539275593"),
-        .package(url: "https://github.com/swift-foundations/swift-io.git", branch: "main"),
+        .package(url: "https://github.com/swift-foundations/swift-io.git", revision: "271da4dee53063379776337a6a3b1be21751babb"),
         .package(url: "https://github.com/swift-foundations/swift-sockets.git", revision: "74a57fa388e07e09f985fee89075577993c75c5b"),
         .package(url: "https://github.com/swift-foundations/swift-tls.git", revision: "e27e99f5c841170593dde7b0396e9090a7515f62"),
     ],
